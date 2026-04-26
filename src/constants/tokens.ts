eba320ae42823d50d00980ea90cc2f8ef894c037
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-04-26 23:51:51 +0800

    feat: design tokens

---
+++ src/constants/tokens.ts
@@ -0,0 +1,19 @@
+export const tokens = {
+    colors: {
+        bg: '#FFFFFF',
+        textPrimary: '#111416',
+        textSecondary: '#57626F',
+        border: '#E8ECEF',
+        purple: {
+            100: '#D5C9FF',
+            400: '#6115CD',
+            500: '#4E11A4',
+            600: '#3A0D7B',
+        },
+        gray: {
+            100: '#F3F3F3',
+            300: '#DDDDDD',
+        },
+        white: '#FFFFFF',
+    }
+} as const
