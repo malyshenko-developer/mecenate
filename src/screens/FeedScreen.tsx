0f19b6e202d6ad76796c4966ef6177f63ebd03fd
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2026-04-27 17:03:28 +0800

    feat: Error handling in FeedScreen

--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import {View, Text, Button, ActivityIndicator, FlatList, RefreshControl} from 'react-native';
+import {View, Text, Button, ActivityIndicator, FlatList, RefreshControl, TouchableOpacity} from 'react-native';
 import { observer } from 'mobx-react';
+
+import MaskotSvg from '../../assets/icons/maskot.svg';
+
 import {usePosts} from "../hooks/usePosts";
 import PostItem from "../components/PostItem";
+import {tokens} from "../constants/tokens";
 
 const FeedScreen = observer(() => {
     const { data, isLoading, error, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } = usePosts()
 
     if (isLoading) return <ActivityIndicator size={"large"} />;
+
+    if (error) return (
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <MaskotSvg />
+            <Text style={{marginTop: 32, marginBottom: 16, fontSize: 18, lineHeight: 26, fontWeight: "bold", color: tokens.colors.textPrimary}}>Не удалось загрузить публикации</Text>
+            <TouchableOpacity style={{
+                minWidth: 239,
+                backgroundColor: '#6115CD',
+                paddingTop: 8,
+                paddingBottom: 8,
+                paddingHorizontal: 24,
+                borderRadius: 14,
+                alignItems: 'center',
+            }} onPress={() => refetch()}>
+                <Text style={{
+                    color: '#FFFFFF',
+                    fontSize: 15,
+                    lineHeight: 26,
+                    fontWeight: '600'
+                }}
+                >
+                    Повторить
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
 
     const posts = data?.pages.flatMap(page => page.data.posts) || []
 
